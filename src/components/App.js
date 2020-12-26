adc88930decd8302c4ca70ce014a10ea6134cd13
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-27 03:29:19 +0900

    Change file name with fbase

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AppRouter from "components/Router";
-import firebase from "../firebase";
+import fbase from "fbase";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
