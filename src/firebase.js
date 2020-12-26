bf1a3b15cffe0ab92300cf756e03924a9da4478e
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-26 23:38:12 +0900

    Create firebase js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbf2F_Va7XacPFBroe6OUUMRhfsrEGTOA",
+  authDomain: "nwitter-5294a.firebaseapp.com",
+  projectId: "nwitter-5294a",
+  storageBucket: "nwitter-5294a.appspot.com",
+  messagingSenderId: "888518195364",
+  appId: "1:888518195364:web:57912976be0fdb2c6ece31",
+};
+
+export default firebase.initializeApp(firebaseConfig);
