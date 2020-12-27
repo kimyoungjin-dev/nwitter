48462e4dc8e3505d2c165e52e17fe567994059b2
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-27 17:23:29 +0900

    Correct a mistake

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
 }
 
 export default App;
+
+//console.log(authService.currentUser);
+
+//setInterval(() => {
+//console.log(authService.currentUser);
+//}, 2000);
