0956bdb77e0bb54490c84c733904a378ab02d592
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-27 12:55:04 +0900

    load authservice

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import AppRouter from "components/Router";
-import fbase from "fbase";
+import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
     <>
       <AppRouter isLoggedIn={isLoggedIn} />
