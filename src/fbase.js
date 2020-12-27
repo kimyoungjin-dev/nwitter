58d9f22f4fded84f013821c37cc6f375faf31d9e
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-27 12:55:35 +0900

    Use of auth service

--- src/fbase.js
+++ src/fbase.js
@@ -11,4 +11,5 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-export default firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+export const authService = firebase.auth();
