fefdad0b1046ceaf09245b12624672592f32607c
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-27 18:32:58 +0900

    Add profile route

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
@@ -14,11 +20,16 @@ const AppRouter = ({ isLoggedIn }) => {
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
     </Router>
