92fffd4d09393ac8705cbaf3f5d594fac6b5689d
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-28 23:26:10 +0900

    Display name on the screen

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,31 @@ function App() {
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.id,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.id,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          isLoggedIn={Boolean(userObj)}
+          refreshUser={refreshUser}
+          userObj={userObj}
+        />
       ) : (
         "Initializing..."
       )}
