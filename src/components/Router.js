4b61484be322844d4745093938779c43b8869e5e
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-28 21:19:53 +0900

    Add user object

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <Router>
-      {isLoggedIn && <Navigation />}
+      {isLoggedIn && <Navigation userObj={userObj} />}
       <Switch>
         {isLoggedIn ? (
           <>
@@ -16,11 +16,12 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile userObj={userObj} />
             </Route>
           </>
         ) : (
           <>
+            Route
             <Route exact path="/">
               <Auth />
             </Route>
