a797189822120c8d4c27d179e9453e0e0eee4fef
kimyoungjin-dev <75160032+kimyoungjin-dev@users.noreply.github.com>
2020-12-28 19:34:14 +0900

    Import storageservice and use refFromURL

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,4 +1,4 @@
-import { dbService } from "fbase";
+import { dbService, storageService } from "fbase";
 import React, { useState } from "react";
 
 const Nweet = ({ nweetObj, isOwner }) => {
@@ -8,6 +8,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
     if (ok) {
       await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await storageService.refFromURL(nweetObj.attachmentUrl).delete();
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
